65576016d34a488107c8a4e726e6f51526bf6522
Jhon D <jhonpaco@frba.utn.edu.ar>
2022-09-20 15:54:54 -0300

    added environment variables

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const PORT = process.env.PORT || 3000
 
-export const DB_HOST = process.env.DB_HOST || 'localhost'
+export const DB_HOST = process.env.DB_HOST || 'containers-us-west-81.railway.app'
 export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'faztpassword'
-export const DB_NAME = process.env.DB_NAME || 'usersdb'
-export const DB_PORT = process.env.DB_PORT || 3306
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'dl1ocp0BIT77j3U1LI0r'
+export const DB_NAME = process.env.DB_NAME || 'railway'
+export const DB_PORT = process.env.DB_PORT || 7988
